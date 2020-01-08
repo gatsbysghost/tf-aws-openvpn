b52ab4b168505eeed4db348fbdf8320f3aacf3cb
Scott Reu <sreu@actbluetech.com>
2020-01-08 09:36:25 -0700

    Re-comment all modules which are not (yet) implemented for our ASG

--- aws_route53_record.public.tf
+++ aws_route53_record.public.tf
@@ -1,18 +1,10 @@
 # ROUTE53 PUBLIC DNS
 // TODO
-resource "aws_route53_record" "public" {
-  zone_id = var.openvpn_route53_public_zone_id
-  name    = var.openvpn_public_dns
-  type    = "A"
-  ttl     = "300"
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibility in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  records = [local.public_ip]
-}
+//resource "aws_route53_record" "public" {
+//  zone_id = var.openvpn_route53_public_zone_id
+//  name    = var.openvpn_public_dns
+//  type    = "A"
+//  ttl     = "300"
+//  records = [local.public_ip]
+//}
 
--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
-output "instance_id" {
-  value = aws_instance.openvpn.id
-}
-
-output "availability_zone" {
-  value = aws_instance.openvpn.availability_zone
-}
-
-output "subnet_id" {
-  value = aws_instance.openvpn.subnet_id
-}
-
-output "private_dns" {
-  value = aws_instance.openvpn.private_dns
-}
-
-output "private_ip" {
-  value = aws_instance.openvpn.private_p
-}
-
-output "public_ip" {
-  value = local.public_ip
-}
+//output "instance_id" {
+//  value = aws_instance.openvpn.id
+//}
+//
+//output "availability_zone" {
+//  value = aws_instance.openvpn.availability_zone
+//}
+//
+//output "subnet_id" {
+//  value = aws_instance.openvpn.subnet_id
+//}
+//
+//output "private_dns" {
+//  value = aws_instance.openvpn.private_dns
+//}
+//
+//output "private_ip" {
+//  value = aws_instance.openvpn.private_p
+//}
+//
+//output "public_ip" {
+//  value = local.public_ip
+//}
 
